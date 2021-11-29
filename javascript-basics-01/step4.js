e9a402d371c75df6ba24c23e30a313e9479f7306
Assia ahmad <ahmadassia676@gmail.com>
2021-11-29 11:28:24 -0800

    basics1/step4 completed

---
+++ javascript-basics-01/step4.js
@@ -0,0 +1,5 @@
+
+var name = prompt("what is your name?");
+var surname = prompt("what is your surname?");
+var city = prompt("which are you");
+alert( "Name : " + name + "\n" + "surname : " + surname + "\n" + "City : " + city); 
